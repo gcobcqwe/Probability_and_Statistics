cdcf747af932b92ab103c883406aadb6828d2bea
jim <gcobcqwe@yahoo.com.tw>
2015-06-19 21:36:47 +0800

    this math Composite Midpoint Rule  Composite Trapezoidal Rule Composite Simpson’s Rule

---
+++ hw4_f.java
@@ -0,0 +1,100 @@
+package mathhwfour;
+import java.util.*;
+
+public class hw4_f {
+	public static void main(String args[]){
+		double start_point=-1;
+		double end_point=1;
+		
+		Midpoint(start_point,end_point);
+		Trapezoidal(start_point,end_point);
+		Simpson(start_point,end_point);
+	}
+	
+	//midpoint
+	public static void Midpoint(double start_point,double end_point){
+		int anser_m;
+		double anser_value=0;
+		double temp = 0;
+		int n;
+		double h;
+		
+		for(int m=1;;m++){
+		    n=2*m-2;	
+		    h=(end_point-start_point)/(n+2);
+		    for(int j=0;j<=n/2;j++){
+		    	temp+=Math.PI/((1+(start_point+(2*j+1)*h)*(start_point+(2*j+1)*h))*(1+(start_point+(2*j+1)*h)*(start_point+(2*j+1)*h)));
+		    }	    
+		    //must add abs
+		    if(Math.abs(2*h*temp-anser_value)<0.000001){
+		    	anser_m=m;
+		    	anser_value=2*h*temp;
+		    	System.out.println("midpoint:");
+		    	System.out.println("m = "+anser_m);
+		    	System.out.println("final approximation = "+anser_value);
+		    	break;
+		    }
+		    anser_value=2*h*temp;
+		    temp=0;
+		}	
+	} 
+	//Trapezoidal
+	public static void Trapezoidal(double start_point,double end_point){
+		int anser_m;
+		double anser_value=0;
+		double temp = 0;
+		int n;
+		double h;
+		
+		for(int m=1;;m++){
+		    n=m;	
+		    h=(end_point-start_point)/n;
+		    for(int j=1;j<=n-1;j++){
+		    	temp+=Math.PI/((1+(start_point+(j*h))*(start_point+(j*h)))*(1+(start_point+(j*h))*(start_point+(j*h))));
+		    }	    
+		    //System.out.println(Math.abs((((2*temp)+Math.esp(start_point)+Math.esp(end_point))*h/2)));
+		    if(Math.abs((((2*temp)+Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/2)-anser_value)<0.000001 && m!=1){
+		    	anser_m=m;
+		    	anser_value=(((2*temp)+Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/2);
+		    	System.out.println("Trapezoidal:");
+		    	System.out.println("m = "+anser_m);
+		    	System.out.println("final approximation = "+anser_value);
+		    	break;
+		    }
+		    anser_value=(((2*temp)+Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/2);
+		    temp=0;
+		}	 
+	}
+	//Simpson
+	public static void Simpson(double start_point,double end_point){
+		int anser_m;
+		double anser_value=0;
+		double temp_mid = 0;
+		double temp_tra = 0;
+		int n;
+		double h;
+		
+		for(int m=1;;m++){
+		    n=2*m;	
+		    h=(end_point-start_point)/n;
+		    for(int j=1;j<=(n/2);j++){
+		    	temp_mid+=Math.PI/((1+(start_point+((2*j-1)*h))*(start_point+((2*j-1)*h)))*(1+(start_point+((2*j-1)*h))*(start_point+((2*j-1)*h))));
+		    }	    
+		    for(int j=1;j<=(n/2)-1;j++){
+		    	temp_tra+=Math.PI/((1+(start_point+(2*j*h))*(start_point+(2*j*h)))*(1+(start_point+(2*j*h))*(start_point+(2*j*h))));
+		    }	     
+		    //must add abs
+		    if(Math.abs((Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+(4*temp_mid)+(2*temp_tra)+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/3-anser_value)<0.000001 && m!=1){
+		    	anser_m=m;
+		    	anser_value=(Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+(4*temp_mid)+(2*temp_tra)+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/3;
+		    	System.out.println("Simpson's:");
+		    	System.out.println("m = "+anser_m);
+		    	System.out.println("final approximation = "+anser_value);
+		    	break;
+		    }
+		    anser_value=(Math.PI/((1+(start_point)*(start_point))*(1+(start_point)*(start_point)))+(4*temp_mid)+(2*temp_tra)+Math.PI/((1+(end_point)*(end_point))*(1+(end_point)*(end_point))))*h/3;
+		    temp_mid=0;
+		    temp_tra=0;
+		}	
+	} 
+}
